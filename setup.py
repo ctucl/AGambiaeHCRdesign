0540d95b96c71e61c388369ad1d3d13c78e35372
ct_ucl <c.trivedi@ucl.ac.uk>
2025-05-14 17:50:28 +0100

    Overwrite repository with new project structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AGambiaeHCRdesign",  # Replace with your package name
+    version="0.1.0",  # Initial version
+    packages=['AGambiaeHCRdesign'],  # Automatically discover Python packages
+    install_requires=[  # Add dependencies here
+        "Bio<=1.8.0"
+        "biopython<=1.8.0",  # Example dependency
+        "pandas"  # Example dependency
+    ],
+    author="Chintan Trivedi",
+    author_email="c.trivedi@ucl.ac.uk",
+    description="HCR probe designer for A.Gambiae",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ctucl/AGambiaeHCRdesign",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
